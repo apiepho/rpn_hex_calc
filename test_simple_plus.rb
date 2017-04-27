e05b5829ef9fa2acdf89e343d09ad746ac107318
apiepho <alpiepho@gmail.com>
2017-04-27 09:23:49 -0600

    adding simple add

--- test_simple_plus.rb
+++ test_simple_plus.rb
@@ -10,4 +10,88 @@ def test_SimplePlus
   puts "SUITE: test_SimplePlus"
   # hex/dec, op modes, chs
   #puts "  TEST: (hex) check disabled buttons"
+  puts "  TEST: (hex) AB enter, CD enter, plus"
+  click("buttonA")
+  click("buttonB")
+  click("buttonEnter")
+  click("buttonC")
+  click("buttonD")
+  click("buttonEnter")
+  click("buttonPlus")
+  assertResultVal("line0", (0xAB + 0xCD))
+
+  click("buttonClrAll")
+  assertResultEmp("line0")
+
+  puts "  TEST: (hex) AB enter, CD, plus"
+  click("buttonA")
+  click("buttonB")
+  click("buttonEnter")
+  click("buttonC")
+  click("buttonD")
+  click("buttonPlus")
+  assertResultVal("line0", (0xAB + 0xCD))
+
+  click("buttonClrAll")
+  assertResultEmp("line0")
+
+  puts "  TEST: toggle format to %d"
+  $fmtHex = false
+  click("buttonFormat")
+
+  puts "  TEST: (dec) 12 enter, 34 enter, plus"
+  click("button1")
+  click("button2")
+  click("buttonEnter")
+  click("button3")
+  click("button4")
+  click("buttonEnter")
+  click("buttonPlus")
+  assertResultVal("line0", (12 + 34))
+
+  click("buttonClrAll")
+  assertResultEmp("line0")
+
+  puts "  TEST: (dec) 12 enter, 34, plus"
+  click("button1")
+  click("button2")
+  click("buttonEnter")
+  click("button3")
+  click("button4")
+  click("buttonPlus")
+  assertResultVal("line0", (12 + 34))
+
+  click("buttonClrAll")
+  assertResultEmp("line0")
+
+  puts "  TEST: (dec) 12, +/-, enter, 34 enter, plus"
+  click("button1")
+  click("button2")
+  click("buttonChs")
+  click("buttonEnter")
+  click("button3")
+  click("button4")
+  click("buttonEnter")
+  click("buttonPlus")
+  assertResultVal("line0", (-12 + 34))
+
+  click("buttonClrAll")
+  assertResultEmp("line0")
+
+  puts "  TEST: (dec) 12, +/-, enter, 34, plus"
+  click("button1")
+  click("button2")
+  click("buttonChs")
+  click("buttonEnter")
+  click("button3")
+  click("button4")
+  click("buttonPlus")
+  assertResultVal("line0", (-12 + 34))
+
+  click("buttonClrAll")
+  assertResultEmp("line0")
+
+  puts "  TEST: toggle format to %x"
+  $fmtHex = true
+  click("buttonFormat")
 end
